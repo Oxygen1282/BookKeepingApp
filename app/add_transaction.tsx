cbd9e7a02042733eb0e4ea16e0eb169d58c471ef
Oxygen1282 <dzarkovacki03@gmail.com>
2026-03-19 10:53:23 -0600

    Renamed modal to add_transactions.tsx, created remove_transactions.tsx, I installed anaimation packages that will be used later. I created a bable.config.js file that will be neccessary for later animations. Editied index.tsx. Everything Should be working now

--- app/add_transaction.tsx
+++ app/add_transaction.tsx
@@ -38,7 +38,7 @@ export default function ModalScreen() {
         style={styles.input}
       />
       <Button title="Add Transaction" onPress={addNumber} />
-      <Button title="Cancel" onPress={() => router.back()} /> 
+      <Button title="Cancel" onPress={() => router.back()} />
     </View>
   );
 }
@@ -46,44 +46,14 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
-  input: { 
+  input: {
     borderWidth: 1,
     padding: 10,
     width: 200,
-    marginBottom: 20 
+    marginBottom: 20,
   },
 });
-
-// import { Link } from 'expo-router';
-// import { StyleSheet } from 'react-native';
-
-// import { ThemedText } from '@/components/themed-text';
-// import { ThemedView } from '@/components/themed-view';
-
-// export default function ModalScreen() {
-//   return (
-//     <ThemedView style={styles.container}>
-//       <ThemedText type="title">This is a modal</ThemedText>
-//       <Link href="/" dismissTo style={styles.link}>
-//         <ThemedText type="link">Go to home screen</ThemedText>
-//       </Link>
-//     </ThemedView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     padding: 20,
-//   },
-//   link: {
-//     marginTop: 15,
-//     paddingVertical: 15,
-//   },
-// });
